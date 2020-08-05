fafab6a1e43f7eecd40e840bb59c051d66111fad
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-05 20:02:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,6 +18,6 @@ def using_delete(array,string)
   array.delete(string)
 end
 
-def using_delet_at(array,integer)
-  array.delet_at[integer]
+def using_delete_at(array,integer)
+  array.delete_at[integer]
 end
